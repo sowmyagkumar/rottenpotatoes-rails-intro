155f3921e91da6b9821abac874f173c239fdecfb
root <root@ip-172-31-27-197.us-east-2.compute.internal>
2018-09-24 00:56:56 +0000

    part 1 complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    def sortable(column, title = nil)
+        title ||= column.titleize
+        link_to title, :sort => column
+    end
+    def helper_class(field)
+        if(params[:sort].to_s == field)
+            return 'hilite'
+        end
+        return nil
+    end
 end
