5caf5a6470894fe6b6b2fd307a1ba7027209b090
root <root@ip-172-31-27-197.us-east-2.compute.internal>
2018-09-25 03:11:24 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -26,7 +26,7 @@ class MoviesController < ApplicationController
 
   def create
     @movie = Movie.create!(movie_params)
-    flash[:notice] = "#{@movie.title} was successfully created."
+    flash.keep[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
 
@@ -37,14 +37,14 @@ class MoviesController < ApplicationController
   def update
     @movie = Movie.find params[:id]
     @movie.update_attributes!(movie_params)
-    flash[:notice] = "#{@movie.title} was successfully updated."
+    flash.keep[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
 
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
-    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    flash.keep[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
 
